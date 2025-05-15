30dc6ff34b0365d7286ab91a82ad109f81ce6220
giancarlopallotti <106991207+giancarlopallotti@users.noreply.github.com>
2025-05-15 11:20:21 +0200

    Update scroll-area.tsx

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -1,44 +1,20 @@
-// src/components/ui/scroll-area.tsx
-"use client"
+"use client";
 
-import * as React from "react"
-import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
+import { cn } from "@/lib/utils";
 
-/**
- * Wrapper attorno al Radix ScrollArea che mette insieme
- * Viewport + Scrollbar verticali e orizzontali + Corner
- */
-const ScrollArea = React.forwardRef<
+export const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
 >(({ className, children, ...props }, ref) => (
-  <ScrollAreaPrimitive.Root
-    ref={ref}
-    className={cn("relative overflow-hidden", className)}
-    {...props}
-  >
+  <ScrollAreaPrimitive.Root ref={ref} className={cn("overflow-auto", className)} {...props}>
     <ScrollAreaPrimitive.Viewport className="w-full h-full">
       {children}
     </ScrollAreaPrimitive.Viewport>
-
-    <ScrollAreaPrimitive.Scrollbar
-      orientation="vertical"
-      className="flex touch-none select-none p-2"
-    >
-      <ScrollAreaPrimitive.Thumb className="flex-1 bg-gray-300 rounded-full before:absolute before:inset-0 before:m-auto before:min-h-[44px] before:min-w-[44px]" />
+    <ScrollAreaPrimitive.Scrollbar orientation="vertical" className="w-2 bg-transparent">
+      <ScrollAreaPrimitive.Thumb className="bg-scrollbar-thumb rounded-full" />
     </ScrollAreaPrimitive.Scrollbar>
-
-    <ScrollAreaPrimitive.Scrollbar
-      orientation="horizontal"
-      className="flex touch-none select-none p-2"
-    >
-      <ScrollAreaPrimitive.Thumb className="flex-1 bg-gray-300 rounded-full before:absolute before:inset-0 before:m-auto before:min-h-[44px] before:min-w-[44px]" />
-    </ScrollAreaPrimitive.Scrollbar>
-
-    <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
-))
-ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName
-
-export { ScrollArea }
+));
+ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName;
