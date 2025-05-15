22dfb09e383908dfb65078f0813d396a8f2b727c
giancarlopallotti <106991207+giancarlopallotti@users.noreply.github.com>
2025-05-15 11:19:05 +0200

    Update tooltip.tsx

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,44 +1,27 @@
-// src/components/ui/tooltip.tsx
-"use client"
+"use client";
 
-import * as React from "react"
-import * as TooltipPrimitive from "@radix-ui/react-tooltip"
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import { cn } from "@/lib/utils";
 
-/**
- * Semplice API sugar per Tooltip di Radix:
- *  - Tooltip
- *  - TooltipTrigger
- *  - TooltipContent
- */
-const Tooltip = TooltipPrimitive.Root
-const TooltipTrigger = TooltipPrimitive.Trigger
+export const TooltipProvider = TooltipPrimitive.Provider;
+export const Tooltip = TooltipPrimitive.Root;
+export const TooltipTrigger = TooltipPrimitive.Trigger;
 
-const TooltipContent = React.forwardRef<
+export const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
->(({ className, side = "top", children, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <TooltipPrimitive.Portal>
     <TooltipPrimitive.Content
       ref={ref}
       className={cn(
-        "z-50 rounded-md px-2 py-1 text-sm text-white bg-black/75 data-[state=open]:animate-in data-[state=closed]:animate-out",
-        {
-          "translate-y-1": side === "bottom",
-          "-translate-y-1": side === "top",
-          "translate-x-1": side === "left",
-          "-translate-x-1": side === "right",
-        },
+        "rounded-md border bg-popover p-2 text-sm shadow-lg",
         className
       )}
-      side={side}
+      sideOffset={4}
       {...props}
-    >
-      {children}
-      <TooltipPrimitive.Arrow className="fill-black/75" />
-    </TooltipPrimitive.Content>
+    />
   </TooltipPrimitive.Portal>
-))
-TooltipContent.displayName = TooltipPrimitive.Content.displayName
-
-export { Tooltip, TooltipTrigger, TooltipContent }
+));
+TooltipContent.displayName = TooltipPrimitive.Content.displayName;
